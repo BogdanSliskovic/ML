5e0af9ab83272d4d6dc3186ea3596798d2786748
BogdanSliskovic <bogdansliskovic@gmail.com>
2025-03-19 15:01:28 +0100

    19/03 RZS

---
+++ proveriGit.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import os
+
+# Putanja do Git repozitorijuma
+repo_path = r"C:\Users\bogdan.sliskovic\Desktop\bole\ML"
+
+# Prelazak u repozitorijum
+os.chdir(repo_path)
+
+# Fetch-uje izmene sa remote-a (da vidi šta se promenilo)
+print("📢 Proveravam promene u repozitorijumu...\n")
+os.system("git fetch")
+
+# Prikazuje detaljan status i promene fajlova
+print("\n📝 *** STATUS REPOZITORIJUMA ***\n")
+os.system("git status")
+
+print("\n🔍 *** DETALJNE PROMENE ***\n")
+os.system("git diff --stat")  # Prikazuje listu promenjenih fajlova
+
+# Pita korisnika da li želi da pull-uje izmene
+odgovor = input("\n❓ Želiš li da pull-uješ najnovije promene? (da/ne): ").strip().lower()
+
+if odgovor in ["da", "d", "yes", "y"]:
+    print("\n📥 Povlačim najnovije izmene sa Gita...\n")
+    os.system("git pull")
+else:
+    print("\n🚫 Povlačenje izmena je preskočeno.")
+
