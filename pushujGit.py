89a88f583fff5b01a269a75e1303a4247c909066
BogdanSliskovic <bogdansliskovic@gmail.com>
2025-03-24 20:30:23 +0100

    2025-03-24 MRJV

---
+++ pushujGit.py
@@ -0,0 +1,14 @@
+import os
+from datetime import date
+
+repo_path = r'C:\Users\Jelena\Desktop\Freestyle\GitHub\ML'
+
+os.chdir(repo_path)
+os.system("git add .")
+
+today = date.today()
+
+x = input('Kako zelis da nazoves commit?')
+os.system(f'git commit -m"{today} {x}"')
+
+os.system("git pull")
