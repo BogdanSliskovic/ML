ab6b3a0479b34a43d142f613458300270a1545a1
BogdanSliskovic <bogdansliskovic@gmail.com>
2025-07-16 21:23:07 +0200

    1607 MRJV

--- film/inference.py
+++ film/inference.py
@@ -5,37 +5,22 @@ from tensorflow import keras
 import joblib
 import matplotlib.pyplot as plt
 import numpy as np
-model = keras.models.load_model("proba.keras", custom_objects={
+model = keras.models.load_model("model_128_128_64_32.keras", custom_objects={
     "L2NormalizeLayer": L2NormalizeLayer,
     "SqueezeLayer": SqueezeLayer,
     "MovieRecommender": MovieRecommender
 })
 
-raw_dataset = tf.data.TFRecordDataset('dataset.tfrecord')
-
-
 history = joblib.load('histori_128_128_64_32.pkl')
-norm_user = keras.models.load_model('norm_user.keras')
-norm_movies = keras.models.load_model('norm_movies.keras')
+norm_user = keras.models.load_model('user_scaler.keras')
+norm_movies = keras.models.load_model('movies_scaler.keras')
 hist = pl.DataFrame(history.history)
 
 test = pl.read_csv('../ml-32m/ratings_test.csv')
 movies = pl.read_csv('../ml-32m/movies.csv')
-data_test = tf.data.Dataset.from_generator(lambda: batch_generator(ratings_path= "../ml-32m/ratings_test.csv", movies_path= '../ml-32m/movies.csv',batch_size= 10000, train = False), output_signature= ((tf.TensorSpec(shape=(None, 22), dtype=tf.float64, name = 'user'), tf.TensorSpec(shape=(None, 25), dtype=tf.float64, name = 'movie')), tf.TensorSpec(shape=(None,3), dtype=tf.float32)))
-userId = 54488
-temp = test.filter(pl.col('userId') == userId)
-u,m, y = prep_pipeline(temp, movies)
-norm_user.variance.numpy()
-u[0, 2:] - norm_user.mean.numpy()
-temp.mean()['rating']
-st = (u[0, 2:].to_numpy() - norm_user.mean.numpy()) / np.sqrt(norm_user.variance)
-st[0,0] = -100
-
-model.user_net(st)
-model.movie_net
-
-m.filter(pl.col('moviId') == m.select(pl.col('movieId')).unique())
-m.is_duplicated()
+data_test = tf.data.Dataset.from_generator(lambda: batch_generator(ratings_path= '../ml-32m/ratings_test.csv', movies_path= '../ml-32m/movies.csv',batch_size= 10000, train = False), output_signature= ((tf.TensorSpec(shape=(None, 22), dtype=tf.float64, name = 'user'), tf.TensorSpec(shape=(None, 25), dtype=tf.float64, name = 'movie')), tf.TensorSpec(shape=(None,3), dtype=tf.float32)))
+next(iter(data_test))
+model.evaluate(data_test)
 
 def spoji_users(df):
     # df shape: (batch, 25)
@@ -69,6 +54,24 @@ data_test = data_test.map(
 ).prefetch(tf.data.AUTOTUNE)
 (u,m), y = next(iter(data_test))
 user = u
+u.to_numpy()
+
+model.evaluate(u, data_test)
+
+userId = 29499
+temp = test.filter(pl.col('userId') == userId)
+u,m, y = prep_pipeline(temp, movies)
+norm_user.variance.numpy()
+u[:, 2:].to_numpy() - norm_user.mean.numpy()
+temp.mean()['rating']
+st = (u[:, 2:].to_numpy() - norm_user.mean.numpy()) / np.sqrt(norm_user.variance)
+st[:,0] = 0
+
+model.user_net(st)
+model.movie_net
+
+m.filter(pl.col('moviId') == m.select(pl.col('movieId')).unique())
+m.is_duplicated()
 
 
 model.user_net.summary()
@@ -140,66 +143,7 @@ val, idx = tf.math.top_k(pred_negledani, k = 10)
 
 
 
-
-
-
-
-
-
-
-tf.random.set_seed(42)
-lista_id = tf.convert_to_tensor(df.select(pl.col('userid').unique()).to_numpy().flatten(), dtype=tf.float32)
-user_ids = tf.random.shuffle(lista_id)[:5]
-
-X_user_full, X_movie_full ,y_full, scalers_full,  = scale(df, user, movies)
-X_user_id, X_movie, y, scalers = scale(df, user, movies, user_id = user_ids)
-X_user = X_user_id[:,1:]
-assert [i for i in scalers if not np.allclose(scalers[i], scalers_full[i])] == []
-assert X_user.shape[0] == X_movie.shape[0] == y.shape[0]
-
-
-user_vec = X_user_id[0, 1:]  # ili kako već uzimaš vektor korisnika
-user_vecs = tf.repeat(tf.reshape(user_vec, (1, -1)), X_movie_full.shape[0], axis=0)
-preds = model.predict([user_vecs, X_movie_full])
-
-# Izbaci filmove koje je korisnik već gledao:
-gledani_movieid = set(df.filter(pl.col('userid') == user_id)['movieid'].to_list())
-svi_movieid = X_movie_full[:, 0].numpy()  # pretpostavka: prva kolona je movieid
-mask = np.array([mid not in gledani_movieid for mid in svi_movieid])
-preds_novi = preds[mask]
-movieid_novi = svi_movieid[mask]
-
-
-model.movie_net
-
-
-
-mse = pl.DataFrame({'mse': history.history['mse'], 'val_mse': history.history['val_mse']})
-plt.figure(figsize = (6,4))
-plt.plot(mse['mse'], label='loss')
-plt.plot(mse['val_mse'], label='val_loss')
-plt.xlabel('Epoch')
-plt.ylabel('MSE')
-plt.show()
-
-
-model.summary()
-preds = model.predict([X_user, X_movie])
-preds.shape
-prvi = int(tf.argmax(preds, axis=0))
-rez = preds[prvi]
-film_id = int(X_user_id[prvi][0])
-df.filter(pl.col('movieid') == film_id).select(['userid', 'movieid', 'rating'])
-
-movies.select(['movieid', 'title']).join(
-    df.filter(pl.col('userid') == id).select(['userid', 'movieid', 'rating']),
-    on='movieid',
-    how='inner'
-)
-
-movies.filter(pl.col('movieid') == film_id).select(['movieid', 'title'])
-
-### inference.py 
+### inference.py
 m_net = model.movie_net
 m_embed = m_net.predict(X_movie)
 print(m_embed.shape)
--- film/train.py
+++ film/train.py
@@ -3,77 +3,74 @@ from model import *
 import tensorflow as tf
 
 from tensorflow import keras
-from keras import layers
-from keras.saving import register_keras_serializable
+
 import joblib
 import tensorflow as tf
 from tensorflow import keras
-import numpy as np
-from keras import layers
-from keras.saving import register_keras_serializable
 import tensorflow as tf
 from tensorflow import keras
 import numpy as np
-from keras import layers
-from keras.saving import register_keras_serializable
+
+
+batch = 2**16 #65536
 
 csv_ratings = "../ml-32m/ratings.csv"
 csv_movies = "../ml-32m/movies.csv"
 
+
+#Podela na train, dev i test u razlicite .csv fajlove
+za_test = (1_000_000 // batch + 1) * batch
+
+df = pl.read_csv(csv_ratings)
+#promesaj df
+dev = pl.read_csv(csv_ratings).sample(za_test, shuffle= True, seed = 42)
+test = pl.read_csv(csv_ratings).sample(za_test, shuffle= True, seed = 42)
+
+dev_test = pl.concat([dev, test])
+
+train = df.join(dev_test, on = ['userId', 'movieId'], how = 'anti')
+train = train.sample(fraction=1.0, shuffle=True, seed = 42) #promesaj train set
+
+for skup in ['train', 'dev', 'test']:
+    temp = globals()[skup]
+    path = f'../ml-32m/ratings_{skup}.csv'
+    temp.write_csv(path)
+    globals()[skup+ '_path'] = path
+
+
+
+data_train = tf.data.Dataset.from_generator(lambda: batch_generator(ratings_path= train_path, movies_path= csv_movies,batch_size= batch, train = True),
+    output_signature= ((tf.TensorSpec(shape=(None, 20), dtype=tf.float64, name = 'user'), tf.TensorSpec(shape=(None, 23),
+    dtype=tf.float64, name = 'movie')), tf.TensorSpec(shape=(None,1), dtype=tf.float32))).prefetch(tf.data.AUTOTUNE)
+
+data_dev = tf.data.Dataset.from_generator(lambda: batch_generator(ratings_path= dev_path, movies_path= csv_movies,batch_size= batch, train = True),
+    output_signature= ((tf.TensorSpec(shape=(None, 20), dtype=tf.float64, name = 'user'), tf.TensorSpec(shape=(None, 23),
+    dtype=tf.float64, name = 'movie')), tf.TensorSpec(shape=(None,1), dtype=tf.float32))).prefetch(tf.data.AUTOTUNE)
+
 user_kolone, movies_kolone = imena_kolona(csv_ratings, csv_movies)
 
-batch = 100 #~65536
+# norm_user = standardizacija(data_train.map(lambda x,y: x[0]))
+# norm_movies = standardizacija(data_train.map(lambda x,y: x[1][:,:3])) # samo prve tri kolone su numericke (#ratings_film, year, avg rating, ostale su dummy)
+# keras.models.save_model(norm_user, 'user_scaler.keras')
+# keras.models.save_model(norm_movies, 'movies_scaler.keras')
+norm_user = keras.models.load_model('user_scaler.keras')
+norm_movies = keras.models.load_model('movies_scaler.keras')
 
-
-data = tf.data.Dataset.from_generator(lambda: batch_generator(ratings_path= csv_ratings, movies_path= csv_movies,batch_size= batch, train = True), output_signature= ((tf.TensorSpec(shape=(None, 20), dtype=tf.float64, name = 'user'), tf.TensorSpec(shape=(None, 23), dtype=tf.float64, name = 'movie')), tf.TensorSpec(shape=(None,1), dtype=tf.float32)))
-
-
-train_size = 2#(10_000_000 // batch) +1 #10_027_008
-dev_size = 1 #(250_000 // batch) + 1 #262144
-#test_size = #(250_000 // batch) + 1 #262144
-
-data_train = data.take(train_size).prefetch(tf.data.AUTOTUNE) #ne stavlja se repeat zbog adapt (beskonacno ga racuna posto nema kraja datasetu)
-
-
-norm_user = standardizacija(data_train.map(lambda x,y: x[0]))
-norm_movies = standardizacija(data_train.map(lambda x,y: x[1][:,:3])) # samo prve tri kolone su numericke (#ratings_film, year, avg rating, ostale su dummy)
-keras.models.save_model(norm_user, '123.keras')
-t = keras.models.load_model('123.keras')
-t.mean
-norm_user
-
+#skaliranje
 data_train = data_train.map(lambda x, y: ((norm_user(x[0]),tf.concat([tf.cast(norm_movies(x[1][:, :3]), tf.float32),tf.cast(x[1][:, 3:], tf.float32)], axis=1)),scale_y(y))).repeat().prefetch(tf.data.AUTOTUNE)
+next(iter(data_train))
+data_dev = data_dev.map(lambda x, y: ((norm_user(x[0]),tf.concat([tf.cast(norm_movies(x[1][:, :3]), tf.float32),tf.cast(x[1][:, 3:], tf.float32)], axis=1)),scale_y(y))).prefetch(tf.data.AUTOTUNE)
 
 model = MovieRecommender(user_input_dim=20,movie_input_dim=23,hidden_layers=[128, 128, 64], embedding_dim=32)
 
 model.compile(optimizer=keras.optimizers.Adam(learning_rate=0.001), loss='mse', metrics=['mae', 'mse'])
 
-data_dev = data.skip(train_size).take(dev_size)
+train_size = train.height // batch
 
-data_dev = data_dev.map(lambda x, y: ((norm_user(x[0]),tf.concat([tf.cast(norm_movies(x[1][:, :3]), tf.float32),tf.cast(x[1][:, 3:], tf.float32)], axis=1)),scale_y(y))).prefetch(tf.data.AUTOTUNE)
-
-
-history = model.fit(data_train, validation_data=data_dev, epochs=5, steps_per_epoch=train_size, validation_steps=dev_size)
+history = model.fit(data_train, validation_data=data_dev, epochs=15, steps_per_epoch=train_size)
 
 
-# model.save(f'model_128_128_64_32.keras')
-keras.models.save_model(norm_user, 'norm_user.keras')
-keras.models.save_model(norm_movies, 'norm_movies.keras')
-# joblib.dump(history, 'histori_128_128_64_32.pkl')
-
-def serialize_example(user, movie, label):
-    feature = {
-        'user': tf.train.Feature(float_list=tf.train.FloatList(value=user)),
-        'movie': tf.train.Feature(float_list=tf.train.FloatList(value=movie)),
-        'label': tf.train.Feature(float_list=tf.train.FloatList(value=label)),
-    }
-    example_proto = tf.train.Example(features=tf.train.Features(feature=feature))
-    return example_proto.SerializeToString()
-
-with tf.io.TFRecordWriter('dataset.tfrecord') as writer:
-    for (x, y) in data_train.take(15):
-        user, movie = x
-        label = y
-        for i in range(user.shape[0]):
-            serialized = serialize_example(user[i].numpy(), movie[i].numpy(), label[i].numpy())
-            writer.write(serialized)
+model.save(f'model_128_128_64_32.keras')
+keras.models.save_model(norm_user, 'user_scaler.keras')
+keras.models.save_model(norm_movies, 'movies_scaler.keras')
+joblib.dump(history, 'histori_128_128_64_32.pkl')
